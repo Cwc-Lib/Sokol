f42df647d255354ace617846ae90825b0ef80eba
Maeiky <maeiky@gmail.com>
2020-09-21 12:57:22 -0400

    Fix

--- sokol-samples/sapp/loadpng-sapp.c
+++ sokol-samples/sapp/loadpng-sapp.c
@@ -12,8 +12,8 @@
 #define HANDMADE_MATH_IMPLEMENTATION
 #define HANDMADE_MATH_NO_SSE
 #include "HandmadeMath.h"
-#include "sokol_gfx.h"
 #include "sokol_app.h"
+#include "sokol_gfx.h"
 #include "sokol_fetch.h"
 #include "sokol_glue.h"
 #include "stb/stb_image.h"
